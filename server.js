f2ab14f8b208bc91a9f0097556a0a23c3cbfdfdd
Aaditya Menon <aaditya@sparker0i.me>
2020-07-25 20:31:01 +0530

    Platform Select Endpoint

--- server.js
+++ server.js
@@ -22,21 +22,33 @@ var connString = `DRIVER={DB2};DATABASE=${db2.db};UID=${db2.username};PWD=${db2.
 
 ibmdb.open(connString, function (err, connection) {
     if (err) {
-        console.log(err.message)
+        console.log(err.message);
     }
     else {
-        dbConnection = connection
+        dbConnection = connection;
     }
 });
 
-app.get('/country-select', function (req, res) {
-    dbConnection.query("SELECT * FROM OTT_TEST.COUNTRY", function(err, result, moreResultSets) {
-        console.log(moreResultSets)
+function queryDatabase(query, req, res) {
+    dbConnection.query(query, function(err, result, moreResultSets) {
+        console.log(moreResultSets);
         
-        if (!err) res.send(result)
-        else res.send(err.message)
+        if (err) res.send(err.message);
+        else {
+            res.setHeader('content-type', 'application/json');
+            res.send(result);
+        }
     })
+}
+
+app.post('/country-select', function (req, res) {
+    queryDatabase("SELECT * FROM OTT_TEST.COUNTRY", req, res);
 });
+
+app.post('/platform-select', function(req, res) {
+    const code = req.body.code;
+    queryDatabase(`SELECT * FROM OTT_TEST.PLATFORM_AVAILABILITY WHERE COUNTRY_CODE = '${code}'`, req, res);
+})
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
