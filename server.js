1d4500f01285001d576f1a033f9b6ee8f2beb15c
Aaditya Menon <aaditya@sparker0i.me>
2020-08-09 19:03:16 +0530

    Added Static Directory Mapping in Server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.json());
+app.use(express.static(__dirname + '/content'));
 app.use(express.urlencoded({
     extended: true
 }));
